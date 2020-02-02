63a30f677503c3ae0b9bc988fbdcda2d3151fc4b
Jenny Li <getjennyli@gmail.com>
2020-02-01 19:28:54 -0500

    Initial Commit

---
+++ 30DaySwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  30DaySwiftUI
+//
+//  Created by Jenny Li on 2/1/20.
+//  Copyright © 2020 Jenny Li. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 30DaySwiftUITests/_0DaySwiftUITests.swift
@@ -0,0 +1,34 @@
+//
+//  _0DaySwiftUITests.swift
+//  30DaySwiftUITests
+//
+//  Created by Jenny Li on 2/1/20.
+//  Copyright © 2020 Jenny Li. All rights reserved.
+//
+
+import XCTest
+@testable import _0DaySwiftUI
+
+class _0DaySwiftUITests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
